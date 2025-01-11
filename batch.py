d05e94a1fdf9e334fd5cfe5e8c94a632abf97615
Tank <63246618+coolrobby@users.noreply.github.com>
2025-01-11 20:57:40 +0800

    Update batch.py

--- batch.py
+++ batch.py
@@ -112,5 +112,5 @@ if st.button("生成语音文件"):
 
 # 侧边栏底部反馈信息
 st.sidebar.markdown("---")
-st.sidebar.write("<h2> 使用说明</h2><p>按组生成单词读音，每个单词读3遍，每组一个mp3文件</p>", unsafe_allow_html=True)
+st.sidebar.write("<h2> 使用说明</h2><p>按组生成单词读音，每个单词读3遍，每组一个mp3文件。选择音色，选择语速后，点击生成语音即可。可以单独下载每个音频，也可以打包下载。</p><p>Made by：川哥</p>", unsafe_allow_html=True)
 
